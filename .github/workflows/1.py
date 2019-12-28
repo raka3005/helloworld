191f28de25672e66bae35b86b9d0ed7a57353de6
raka3005 <59300097+raka3005@users.noreply.github.com>
2019-12-28 18:55:40 +0700

    arkademy test

---
+++ .github/workflows/1.py
@@ -0,0 +1,16 @@
+name = 'Aditya Raka Bagaskara'
+print(name)
+age = 24
+print(age)
+address = 'Bukit Sawangan Indah Blok A4 no19'
+print(address)
+hobbies = ('olahraga', 'nonton film', 'memasak', 'travelling')
+print(hobbies)
+is_married = False
+print(is_married)
+list_school = ('Taruna Nusantara', '2010', '2013', 'IPS')
+print(list_school)
+skills = ('Python', 'Beginner')
+print(skills)
+interest_in_coding = True
+print(interest_in_coding)
